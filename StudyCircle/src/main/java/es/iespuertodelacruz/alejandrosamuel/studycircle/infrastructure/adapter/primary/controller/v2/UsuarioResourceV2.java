e7987f68fa770799045dfc7114af66b6b56abf1a
sgonzalez <sgonzalez@avantic.net>
2023-04-14 07:45:03 +0100

    Corrección petición reenviar correo en controlador Se incluye dentro del condicional que comprueba que el usuario esté en estado pendiente de verificación para reenviar el correo, en caso negativo se devuelve el estado actual del usuario

--- StudyCircle/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/infrastructure/adapter/primary/controller/v2/UsuarioResourceV2.java
+++ StudyCircle/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/infrastructure/adapter/primary/controller/v2/UsuarioResourceV2.java
@@ -8,6 +8,7 @@ import es.iespuertodelacruz.alejandrosamuel.studycircle.infrastructure.adapter.s
 import es.iespuertodelacruz.alejandrosamuel.studycircle.infrastructure.config.EmailSender;
 import es.iespuertodelacruz.alejandrosamuel.studycircle.infrastructure.security.UserDetailsLogin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -32,20 +33,19 @@ public class UsuarioResourceV2 {
     @GetMapping("resendconfirmationemail")
     public ResponseEntity<?> reenviarConfirmacionToken() {
         Usuario usuario = usuarioService.findByUsername(getUsernameUsuario());
-        TokenConfirmacionEntity token = usuarioService.resendConfirmationToken(usuario);
-        String link = "http://localhost:8080/api/register/confirm?token=" + token.getToken();
-        emailSender.enviar(
-                usuario.getEmail(),
-                htmlBuilder.buildEmail(usuario.getNombreCompleto(), link));
-        if(usuario.getEstado().equals(EstadosUsuario.STATUS_PENDING_VERIFICATION)) {
 
+        if(usuario.getEstado().equals(EstadosUsuario.STATUS_PENDING_VERIFICATION.name())) {
+            TokenConfirmacionEntity token = usuarioService.resendConfirmationToken(usuario);
+            String link = "http://localhost:8080/api/register/confirm?token=" + token.getToken();
+            emailSender.enviar(
+                    usuario.getEmail(),
+                    htmlBuilder.buildEmail(usuario.getNombreCompleto(), link));
         }
-        return null;
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(usuario.getEstado());
     }
 
     private String getUsernameUsuario() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String nombreAutenticado = ((UserDetailsLogin) principal).getUsername();
-        return nombreAutenticado;
+        return ((UserDetailsLogin) principal).getUsername();
     }
 }
