e07827800cb43651e50ce36b7cd768efffb02acc
Alejandro Pérez Martín <aleperzmartin@gmail.com>
2023-04-27 22:29:32 +0100

    Se añade una versión inicial de la  activity de configuracion del perfil del tutor..
    Se encuentran diferentes retos al intentar hacer el diseño propuesto en los mockups. Se está investigando como hacerlos.

---
+++ StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ConfiguracionPerfilTutorActivity.java
@@ -0,0 +1,14 @@
+package es.iespuertodelacruz.alejandrosamuel.studycircle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfiguracionPerfilTutorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracion_perfil_tutor);
+    }
+}
