21a451b9064e35cc7c2fb1a927d5331859fd7338
Alejandro Pérez Martín <aleperzmartin@gmail.com>
2023-05-01 00:51:28 +0100

    Ajustes  de las views, intents para las previews y modelo base de la vista de configuracion del perfil, buscando la mejor combinación o forma de hacerlo, a confirmar mañana al hablar con mi compañero.

--- StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/ConfiguracionPerfilTutorActivity.java
+++ StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/ConfiguracionPerfilTutorActivity.java
@@ -2,15 +2,31 @@ package es.iespuertodelacruz.alejandrosamuel.studycircle.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import es.iespuertodelacruz.alejandrosamuel.studycircle.R;
+import es.iespuertodelacruz.alejandrosamuel.studycircle.databinding.ActivityConfiguracionPerfilTutorBinding;
 
 public class ConfiguracionPerfilTutorActivity extends AppCompatActivity {
+
+    private ActivityConfiguracionPerfilTutorBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion_perfil_tutor);
+        binding = ActivityConfiguracionPerfilTutorBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.btnSalir4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/RegisterActivity.java
+++ StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/RegisterActivity.java
@@ -2,15 +2,31 @@ package es.iespuertodelacruz.alejandrosamuel.studycircle.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import es.iespuertodelacruz.alejandrosamuel.studycircle.R;
+import es.iespuertodelacruz.alejandrosamuel.studycircle.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.btnSalir3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
