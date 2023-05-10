79afce71da0f526b8fed3b3d84e5699e314f00db
Alejandro Pérez Martín <aleperzmartin@gmail.com>
2023-05-10 20:16:48 +0100

    Se añaden los cambios para la nueva navegación. A falta de añadir estos en los fragments para hacer el movimiento mas dinamico.

---
+++ StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/home/FragmentHomeViewModel.java
@@ -0,0 +1,7 @@
+package es.iespuertodelacruz.alejandrosamuel.studycircle.ui.home;
+
+import androidx.lifecycle.ViewModel;
+
+public class FragmentHomeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/home/HomeFragment.java
+++ StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/home/HomeFragment.java
@@ -6,43 +6,70 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
-import androidx.navigation.NavHost;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import es.iespuertodelacruz.alejandrosamuel.studycircle.R;
 
 import es.iespuertodelacruz.alejandrosamuel.studycircle.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+    private FragmentHomeViewModel mViewModel;
     private FragmentHomeBinding binding;
-    private  NavController navController;
+    private NavController navController;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        /*
+        System.out.println(binding);
 
         BottomNavigationView navView = binding.navStart;
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                R.id.navigation_start, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
 
-      //  NavHostFragment navHostFragment = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_home);
-//        navController = navHostFragment.getNavController();
+       NavHostFragment navHostFragment = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_home);
+        navController = navHostFragment.getNavController();
+         NavController navController = Navigation.findNavController(this.requireActivity(), R.id.nav_host_fragment_activity_home); // Problem #2 .findNavController
+        NavigationUI.setupActionBarWithNavController((AppCompatActivity) this.requireActivity(), navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
 
-        //NavController navController = Navigation.findNavController(this.requireActivity(), R.id.nav_host_fragment_activity_home); // Problem #2 .findNavController
-     //  NavigationUI.setupActionBarWithNavController((AppCompatActivity) this.requireActivity(), navController, appBarConfiguration);
-      // NavigationUI.setupWithNavController(navView, navController);
+*/      binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        return binding.getRoot();
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+
+
+        binding.btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View viewButton) {
+                NavController navController = Navigation.findNavController(binding.getRoot());
+                navController.navigate(R.id.action_nav_home_to_navigation_start);
+            }
+        });
+
+        binding.btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View viewButton) {
+                NavController navController = Navigation.findNavController(binding.getRoot());
+                navController.navigate(R.id.action_nav_home_to_navigation_notifications);
+            }
+        });
+
+        binding.btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View viewButton) {
+                NavController navController = Navigation.findNavController(binding.getRoot());
+                navController.navigate(R.id.action_nav_home_to_navigation_dashboard);
+            }
+        });
+
+        return root;
     }
 
     @Override
