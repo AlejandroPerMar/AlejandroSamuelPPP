4487243e8835929baeb4f93cfc95cafce713ee74
sgonzalez <sgonzalez@avantic.net>
2023-05-17 15:08:43 +0100

    Se realizan las vistas de Inicio de Sesión, Registro y se comienza la de verificación de cuenta, con la navegación creada entre las dos primeras

--- StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/ActivacionCuentaFragment.java
+++ StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/ActivacionCuentaFragment.java
@@ -1,35 +1,32 @@
 package es.iespuertodelacruz.alejandrosamuel.studycircle.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import es.iespuertodelacruz.alejandrosamuel.studycircle.R;
-import es.iespuertodelacruz.alejandrosamuel.studycircle.databinding.FragmentLoginBinding;
+import androidx.fragment.app.Fragment;
+
+import es.iespuertodelacruz.alejandrosamuel.studycircle.databinding.FragmentActivacionCuentaBinding;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link ActivacionCuentaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
+public class ActivacionCuentaFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private FragmentLoginBinding binding;
+    private FragmentActivacionCuentaBinding binding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public LoginFragment() {
+    public ActivacionCuentaFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +36,11 @@ public class LoginFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * @return A new instance of fragment ActivacionCuentaFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static ActivacionCuentaFragment newInstance(String param1, String param2) {
+        ActivacionCuentaFragment fragment = new ActivacionCuentaFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,15 +61,7 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentLoginBinding.inflate(inflater, container, false);
-
-        binding.btnNavRegistrarse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registerFragment);
-            }
-        });
-
+        binding = FragmentActivacionCuentaBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 }
--- StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/MainActivity.java
+++ StudyCircleApp/app/src/main/java/es/iespuertodelacruz/alejandrosamuel/studycircle/ui/MainActivity.java
@@ -3,23 +3,7 @@ package es.iespuertodelacruz.alejandrosamuel.studycircle.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import es.iespuertodelacruz.alejandrosamuel.studycircle.R;
-import es.iespuertodelacruz.alejandrosamuel.studycircle.data.rest.dto.AlumnoDTO;
-import es.iespuertodelacruz.alejandrosamuel.studycircle.data.rest.dto.CursoDTO;
-import es.iespuertodelacruz.alejandrosamuel.studycircle.data.rest.dto.MateriaDTO;
-import es.iespuertodelacruz.alejandrosamuel.studycircle.data.rest.dto.MateriaTutorDTO;
-import es.iespuertodelacruz.alejandrosamuel.studycircle.data.rest.dto.NivelEstudiosDTO;
-import es.iespuertodelacruz.alejandrosamuel.studycircle.data.rest.dto.UsuarioLoginDTO;
-import es.iespuertodelacruz.alejandrosamuel.studycircle.data.rest.dto.UsuarioRegisterDTO;
 import es.iespuertodelacruz.alejandrosamuel.studycircle.databinding.ActivityMainBinding;
 import es.iespuertodelacruz.alejandrosamuel.studycircle.viewmodel.MainActivityViewModel;
 
@@ -31,9 +15,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         setContentView(binding.getRoot());
     }
 }
